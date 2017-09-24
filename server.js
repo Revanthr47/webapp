f0bef616fe86cc1c38f16be1785cbc492ca30991
Revanthr47 <30695246+Revanthr47@users.noreply.github.com>
2017-09-24 20:13:11 +0530

    worked with req.params function

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
+
+
+app.get('/:query',function(req,res){
+var Query = req.params.query;
+res.send('you requsted for '+ Query);
+});
+
+app.get('/article-one', function(req,res){
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/ui/main.js', function(req,res){
+ res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -21,7 +38,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
