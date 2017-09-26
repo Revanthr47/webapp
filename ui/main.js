3e890f32dd5429a42cd3a071146cfd62f1cd174b
Revanthr47 <30695246+Revanthr47@users.noreply.github.com>
2017-09-27 00:31:43 +0530

    Added counter

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var btn = document.getElementById('btn');
+var counter = 0;
+btn.onclick = function() {
+	
+	counter = counter+1;
+	var spArea = document.getElementById('count');
+	spArea.innerHTML = counter.toString();
+};
